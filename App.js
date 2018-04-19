8fbe5118e87cd9d809daa1b5f3949539525245c1
koneclimate <30680676+koneclimate@users.noreply.github.com>
2018-04-19 16:58:15 +0700

    Test App

--- App.js
+++ App.js
@@ -1,43 +1,53 @@
 import React, { Component } from 'react';
-
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
-export default class AnatomyExample extends Component {
-  	render() {
-    	return (
-      	<Container >
-        	<Header >
-          		<Left>
-            		<Button transparent>
-              			<Icon name='menu' />
-            		</Button>
-          		</Left>
-          		<Body>
-           			<Title>Devices</Title>
-          		</Body>
-          		<Right>
-          			<Button transparent>
-              			<Icon name='md-add' />
-            		</Button>
-          		</Right>
-        	</Header>
-        	<Content>
-          		<Text>
-            		This is Content Section
-          		</Text>
-        	</Content>
-        	<Footer>
-          		<FooterTab>
-            		<Button vertical >
-		              	<Icon name="md-outlet" />
-		              	<Text>Devices</Text>
-		            </Button>
-		            <Button vertical active>
-		              	<Icon name="person" />
-		              	<Text>Profile</Text>
-		            </Button>
-          		</FooterTab>
-       	 	</Footer>
-      	</Container>
+import { View } from 'react-native';
+import { Container, Header, Title, Content, Footer,
+     FooterTab, Button, Left, Right, Body, 
+     Icon, Text, List, ListItem, Thumbnail,
+     Drawer, TouchableOpacity,Form, Item, Input, Label} from 'native-base';
+export default class login extends Component {
+    render() {
+      closeDrawer = () => {
+          this.drawer._root.close()
+      };
+      openDrawer = () => {
+          this.drawer._root.open()
+      };
+      return (
+        <Container >
+            <View style={{flex:1 ,backgroundColor: 'powderblue'}} />
+            <View style={{flex:1 ,backgroundColor: 'skyblue'}} >
+                <View style={{flex:4 }} >
+                  <View style={{flex:3 , flexDirection: 'row'}} >
+                      <View style={{flex:1 }} />
+                      <View style={{flex:8 ,flexDirection: 'column'}} >
+                      <Form>
+                        <Item floatingLabel>
+                          <Label>Email</Label>
+                          <Input />
+                        </Item>
+                        <Item floatingLabel last>
+                          <Label>Password</Label>
+                          <Input secureTextEntry={true} />
+                        </Item>
+                      </Form>
+                      <Button full light style={{marginTop:20}}>
+                          <Text>Sign In</Text>
+                      </Button>
+                      
+                      <Button transparent dark >
+                        <Text >Forgot Password?</Text>
+                    </Button>
+                      </View>
+                      <View style={{flex:1 }} />
+                  </View>
+                </View>
+                <View style={{flex:1 ,justifyContent: 'center',}} >
+                    <Button transparent dark>
+                        <Text >Create Account</Text>
+                    </Button>
+                </View>
+            </View>
+        </Container>
     );
   }
 }
